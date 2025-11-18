70741af3234183ab4d98c6fecdc025b8760006e0
Mitch-creates <mitchcreates@proton.me>
2025-11-18 21:09:37 +0100

    Install resend

---
+++ __tests__/signInPage.test.tsx
@@ -0,0 +1,13 @@
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+// import SignIn from "@/app/[locale]/account/signin/page";
+
+// describe("Page", () => {
+//   it("renders a heading", () => {
+//     render(<SignIn />);
+
+//     const heading = screen.getByRole("heading", { level: 1 });
+
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 config();
 // TODO Implement magic link auth https://www.better-auth.com/docs/plugins/magic-link
 // TODO Implement passkey https://www.better-auth.com/docs/plugins/passkey
-// Implement username? https://www.better-auth.com/docs/plugins/username
+// TODO Implement emailVerification https://www.better-auth.com/docs/authentication/email-password#email-verification
+// TODO Implement Forgetpassword https://www.better-auth.com/docs/authentication/email-password#request-password-reset
 
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
