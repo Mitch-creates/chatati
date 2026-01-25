c0240ce1d34ce1de0291a5be2492121e4d4e250b
mitch-creates <michielvandevyver@gmail.com>
2026-01-25 18:35:30 +0100

    Started profile page and 404 when no profile found

--- app/[locale]/platform/profile/[id]/page.tsx
+++ app/[locale]/platform/profile/[id]/page.tsx
@@ -1,14 +1,29 @@
 import { Spinner } from "@/components/ui/spinner";
 import { requireAuthAndEmailVerified } from "@/lib/auth-utils";
+import { getUserWithProfile } from "@/lib/services/user.service";
+import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-export default async function ProfilePage() {
+async function ProfilePageContent({userId, locale}: {userId: string; locale: string}) {
+  const user = await getUserWithProfile(userId);
+
+  if (!user) {
+    notFound();
+  }
+  return (
+    <>
+    </>
+  );
+}
+
+export default async function ProfilePage({params}: {params: Promise<{id: string; locale: string}>}) {
   const session = await requireAuthAndEmailVerified();
-  const user = session.user;
+  const {id, locale} = await params;
+
   
   return (
     <Suspense fallback={<Spinner />}>
-      <div>Profile page for {user.email}</div>
+      <ProfilePageContent userId={id} locale={locale} />
     </Suspense>
   );
 }
